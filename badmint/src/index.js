4cc46e7435d89eac5218f8e86818f26424c70955
Lucas Dolsan <lucas.dolsan1775@gmail.com>
2022-10-11 20:04:12 -0300

    feat: replaced boostrap with MUI in RootLayout

--- badmint/src/index.js
+++ badmint/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import Router from "./pages/Router";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- badmint/src/pages/RootLayout.jsx
+++ badmint/src/pages/RootLayout.jsx
@@ -1,27 +1,60 @@
 import * as React from "react";
 
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import { Outlet } from "react-router-dom";
 
-function RootLayout() {
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const drawerWidth = 240;
+
+export default function RootLayout() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">Badmint</Navbar.Brand>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/import-championship">Importar campeonato</Nav.Link>
-              <Nav.Link href="/import-athlete">Importar atleta</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Outlet />
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Badmint
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          <ListItem key={'import-championship'} disablePadding>
+            <ListItemButton>
+            <ListItemText primary={"Importar campeonato"} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
+    <Outlet />
     </div>
   );
 }
-
-export default RootLayout;
